6379f936b851d0b8d3438d41d6a4217c5dc9d098
Hervé guerre <herveguerre@hotmail.fr>
2023-04-21 16:46:38 +0200

    maj

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router ,Routes ,Route } from 'react-router-dom';
+import StevePage from './component/pages/StevePage';
+import MarkPage from './component/pages/MarkPage';
+import ElonPage from './component/pages/ElonPage';
+import JackPage from './component/pages/JackPage';
+import ErrorPage from './component/pages/ErrorPage';
 
 function App() {
   return (
-    <div className="App">
-      <p>goat</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/'element={<StevePage/>} />
+        <Route path='/mark'element={<MarkPage/>} />
+        <Route path='/elon'element={<ElonPage/>} />
+        <Route path='/jack'element={<JackPage/>} />
+        <Route path='/*'element={<ErrorPage/>} />
+      </Routes>
+    </Router>
+
   );
 }
 
